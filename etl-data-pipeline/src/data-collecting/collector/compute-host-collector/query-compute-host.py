d1afec8e82c073bab6bad61fd804ab66c1e7ad27
bactp <bactp@dcn.ssu.ac.kr>
2023-10-17 11:51:03 +0000

    change query cmd for metrics cluster_instance_mem_utilization

--- etl-data-pipeline/src/data-collecting/collector/compute-host-collector/query-compute-host.py
+++ etl-data-pipeline/src/data-collecting/collector/compute-host-collector/query-compute-host.py
@@ -24,7 +24,7 @@ def compute_host_query(URL):
             'host_cpu_utilization': 'sum(instance:node_cpu_utilisation:rate5m) by (instance)',
             'host_cpu_rate_sum': 'sum(instance:node_cpu:rate:sum) by (instance)',
             'host_load1_per_cpu': 'sum(instance:node_load1_per_cpu:ratio) by (instance)',
-            'host_mem_utilization': 'sum(instance:node_load1_per_cpu:ratio) by (instance)',
+            'host_mem_utilization': 'sum(instance:node_memory_utilisation:ratio) by (instance)',
             'host_netin_bytes_wo_lo': 'sum(instance:node_network_receive_bytes_excluding_lo:rate5m) by (instance)',
             'host_netin_bytes_total': 'sum(instance:node_network_receive_bytes:rate:sum) by (instance)',
             'host_netin_drop_wo_lo': 'sum(instance:node_network_receive_drop_excluding_lo:rate5m) by (instance)',
