695eb3000f7a91e23e43758ae3ae90546cd130cc
bactp <bactp@dcn.ssu.ac.kr>
2023-10-19 10:36:32 +0000

    add try-except for watiting connection to endpoit: avoid crash code in container

--- etl-data-pipeline/src/data-collecting/collector/app-qos-collector/query-app-qos.py
+++ etl-data-pipeline/src/data-collecting/collector/app-qos-collector/query-app-qos.py
@@ -5,6 +5,7 @@ import sys
 import csv
 import os
 from minio import Minio
+import signal
 
 def warehouse_connection():
     client = Minio("192.168.24.20:30256",
@@ -70,13 +71,13 @@ metrics_name = ['service_name',
                 'latency',
                 'timestamp' ]
 
-URL = os.getenv("URL")
-cluster_name =  os.getenv("CLUSTER_NAME") #declare as name of the cluster in container image
-name_space = os.getenv("NAME_SPACE") #declare as name of namespace in container image
+# URL = os.getenv("URL")
+# cluster_name =  os.getenv("CLUSTER_NAME") #declare as name of the cluster in container image
+# name_space = os.getenv("NAME_SPACE") #declare as name of namespace in container image
 
-# URL = "http://192.168.24.20:31179/prometheus/api/v1/query"
-# cluster_name = "data-center1"
-# name_space = "sock-shop"
+URL = "http://192.168.24.20:31179/prometheus/api/v1/query"
+cluster_name = "data-center1"
+name_space = "sock-shop"
 
 
 current_day = None
@@ -84,38 +85,37 @@ csv_file_name = None
 
 
 while True:
-    """ Query data every 10 seconds """
-    
-    now = datetime.datetime.now()
-    current_date = now.strftime("%Y%m%d")
+    try: 
+        now = datetime.datetime.now()
+        current_date = now.strftime("%Y%m%d")
 
-    # Check if the day has changed
-    if current_date != current_day:
-        if csv_file_name:
-            print(f"Closing file: {csv_file_name}")
-            current_day = current_date
+        # Check if the day has changed
+        if current_date != current_day:
+            if csv_file_name:
+                print(f"Closing file: {csv_file_name}")
+                current_day = current_date
 
-            #Upload to storage
-            minioClient = warehouse_connection()
-            path = cluster_name + "_" + name_space + "_app_data/" + current_day
-            minioClient.fput_object(cluster_name, path, csv_file_name, content_type='application/csv')
-            print(f"File: {csv_file_name} is uploaded to storage")
-            os.remove(csv_file_name)
+                #Upload to storage
+                minioClient = warehouse_connection()
+                path = cluster_name + "_" + name_space + "_app_data/" + current_day
+                minioClient.fput_object(cluster_name, path, csv_file_name, content_type='application/csv')
+                print(f"File: {csv_file_name} is uploaded to storage")
+                os.remove(csv_file_name)
 
-            csv_file_name = None
+                csv_file_name = None
 
-        # Create a new CSV file
-        csv_file_name = create_new_csv(metrics_name, cluster_name, name_space)
-        print(f"New file created: {csv_file_name}")
+            # Create a new CSV file
+            csv_file_name = create_new_csv(metrics_name, cluster_name, name_space)
+            print(f"New file created: {csv_file_name}")
 
-    # Simulate writing data to the CSV file
-    with open(csv_file_name, mode='a', newline='') as f:
-                write = csv.writer(f)
-                data = app_qos_query(URL, cluster_name, name_space)
-                write.writerows(data)
-                
-    current_day = current_date
-    time.sleep(10)
-
-         
-
+        # Simulate writing data to the CSV file
+        with open(csv_file_name, mode='a', newline='') as f:
+                    write = csv.writer(f)
+                    data = app_qos_query(URL, cluster_name, name_space)
+                    write.writerows(data)
+                    
+        current_day = current_date
+        time.sleep(10)
+    except Exception as e:
+            print("reconnecting to endpoints")
+            time.sleep(15)
